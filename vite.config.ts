42268d05ebb371544a682ef241c1c975dd36b8be
momotom <a18520145493@gmail.com>
2026-04-20 07:40:14 +0800

    feat: Multi-entry point builds for Vite and package

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
-/// <reference types="vitest/config" />
-import { resolve } from "node:path";
-import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   resolve: {
-    // alias: {
-    //   '@': resolve(__dirname, 'src'),
-    // },
     tsconfigPaths: true,
   },
   plugins: [
@@ -22,18 +17,35 @@ export default defineConfig({
     target: "es2022",
     sourcemap: true,
     lib: {
-      entry: resolve(import.meta.dirname, "src/index.ts"),
-      name: "I18nextReactViteAutoTypes",
+      entry: {
+        index: "src/index.ts", // plugin 主入口
+        "core/index": "src/core/index.ts", // core 主入口
+        "cli/index": "src/cli/index.ts", // cli 主入口
+      },
       formats: ["es", "cjs"],
-      fileName: (format) => (format === "es" ? "index.js" : "index.cjs"),
     },
+    // 外部依赖
     rollupOptions: {
-      external: [],
+      // 显式声明外部依赖，避免打包
+      external: [
+        "vite",
+        "i18next",
+        "react-i18next",
+        "node:fs",
+        "node:path",
+        "node:child_process",
+        /^node:.*/,
+        "@clack/prompts",
+        "picocolors",
+        "chokidar",
+        "i18next-resources-to-backend",
+      ],
       output: {
         exports: "named",
       },
     },
   },
+
   test: {
     globals: true,
     environment: "node",
