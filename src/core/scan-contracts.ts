8836344c7ac5847bb67644304fe65a682a9f043a
momotom <a18520145493@gmail.com>
2026-04-21 22:48:12 +0800

    feat: Filter out non-.ts files and keep only those with the .ts extension

--- src/core/scan-contracts.ts
+++ src/core/scan-contracts.ts
@@ -1,5 +1,7 @@
 import { readdirSync } from "node:fs";
 
 export function scanContracts(dir: string) {
-  return readdirSync(dir);
+  return readdirSync(dir)
+    .filter((file) => file.endsWith(".ts") && !file.endsWith(".d.ts"))
+    .map((file) => file.replace(".ts", ""));
 }
