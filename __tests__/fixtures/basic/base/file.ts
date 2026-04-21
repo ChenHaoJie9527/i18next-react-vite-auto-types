c2145fac9c51fa011a15b29fbcae56b62137f28d
momotom <a18520145493@gmail.com>
2026-04-21 22:42:59 +0800

    feat: Run the script to read the directory

---
+++ __tests__/fixtures/basic/base/file.ts
@@ -0,0 +1,1 @@
+export type FileMessage = { file: string };
---
+++ scripts/dev-run.mjs
@@ -0,0 +1,3 @@
+import { scanContracts } from "../dist/core/index.cjs";
+
+console.log(scanContracts("./__tests__/fixtures/basic/base"));
