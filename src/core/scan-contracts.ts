70ae46c635d7bd33a37576d381b6114149c396ec
momotom <a18520145493@gmail.com>
2026-04-22 21:10:25 +0800

    feat: Validate the returned object

--- src/core/scan-contracts.ts
+++ src/core/scan-contracts.ts
@@ -3,5 +3,10 @@ import { readdirSync } from "node:fs";
 export function scanContracts(dir: string) {
   return readdirSync(dir)
     .filter((file) => file.endsWith(".ts") && !file.endsWith(".d.ts"))
-    .map((file) => file.replace(".ts", ""));
+    .map((file) => {
+      const name = file.replace(".ts", "");
+      return {
+        name,
+      };
+    });
 }
