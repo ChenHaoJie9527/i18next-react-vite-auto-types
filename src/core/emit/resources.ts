4c8aca55ca7ddf4a0048f2fccab6b4dac736b0bf
momotom <a18520145493@gmail.com>
2026-04-22 22:14:43 +0800

    feat: Extend the  function to return a  object

--- src/core/emit/resources.ts
+++ src/core/emit/resources.ts
@@ -17,9 +17,22 @@ interface Namespace {
  * @returns - 返回一个字符串，表示 defaultNS 的值
  */
 export function emitResoureces(namespaces: Namespace[]) {
+  // 新增 resourceNamespaces 对象
+  const entries = namespaces
+    .map((ns) => ` ${ns.name}: {} as ${ns.typeName}`)
+    .join("\n");
+
+  //   console.log("entries:", entries);
+
+  const body = `export const resourceNamespaces = {
+    ${entries}
+  } as const;`;
+
+  //   console.log("body:", body);
+
   // 拼 import 语句列表
   const imports = namespaces
     .map((ns) => `import type ${ns.typeName} from "../base/${ns.name}"`)
     .join("\n");
-  return `${imports}\n\nexport const defaultNS = 'common' as const;\n`;
+  return `${imports}\n\nexport const defaultNS = 'common' as const;\n\n${body}\n`;
 }
