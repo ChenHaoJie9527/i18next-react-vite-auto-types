20eb5f117ccf783b91b31d99515263f995c05d4b
momotom <a18520145493@gmail.com>
2026-04-22 22:05:34 +0800

    feat: Handling dynamic “import type” statements in the  function

--- scripts/dev-run.mjs
+++ scripts/dev-run.mjs
@@ -1,4 +1,4 @@
 import { emitResoureces, scanContracts } from "../dist/core/index.cjs";
 
-console.log(scanContracts("./__tests__/fixtures/basic/base"));
-console.log(emitResoureces());
+// console.log(scanContracts("./__tests__/fixtures/basic/base"));
+console.log(emitResoureces(scanContracts("./__tests__/fixtures/basic/base")));
--- src/core/emit/resources.ts
+++ src/core/emit/resources.ts
@@ -1,7 +1,25 @@
+interface Namespace {
+  name: string;
+  typeName: string;
+}
+
 /**
  * @description 该函数会返回一段合法的 TypeScript 代码，表示 defaultNS 的值
+ * @param namespaces - 命名空间列表
+ * @example
+ * ```ts
+ * const namespaces = [
+ *   { name: 'common', typeName: 'CommonMessage' },
+ *   { name: 'user-management', typeName: 'UserManagementMessage' },
+ * ];
+ * emitResoureces(namespaces);
+ * ```
  * @returns - 返回一个字符串，表示 defaultNS 的值
  */
-export function emitResoureces() {
-  return `export const defaultNS = 'common' as const;\n`;
+export function emitResoureces(namespaces: Namespace[]) {
+  // 拼 import 语句列表
+  const imports = namespaces
+    .map((ns) => `import type ${ns.typeName} from "../base/${ns.name}"`)
+    .join("\n");
+  return `${imports}\n\nexport const defaultNS = 'common' as const;\n`;
 }
