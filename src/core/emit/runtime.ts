70d42fff82a2b09b59d70b8506e058936aba805a
momotom <a18520145493@gmail.com>
2026-04-28 22:31:34 +0800

    feat: Implement the  function so that when given the input , it returns .

--- src/core/emit/runtime.ts
+++ src/core/emit/runtime.ts
@@ -1,4 +1,30 @@
 export function emitRuntime(locales: string[]) {
-  const globPattern = `./{${locales.join(",")}}/*.ts`;
+  const globPattern = createGlobPattern(locales);
   return globPattern;
+}
+
+/**
+ * @description 创建 glob 模式
+ * @param locales - 语言列表
+ * @returns - 返回一个字符串，表示 glob 模式
+ * @example
+ * ```ts
+ * createGlobPattern(["en-US", "zh-CN"]) => "./{en-US,zh-CN}/*.ts"
+ * ```
+ */
+export function createGlobPattern(locales: string[]) {
+  return `./{${locales.join(",")}}/*.ts`;
+}
+
+/**
+ * @description 创建 alternation
+ * @param locales - 语言列表
+ * @returns - 返回一个字符串，表示 alternation
+ * @example
+ * ```ts
+ * createAlternation(["en-US", "zh-CN"]) => "en-US|zh-CN"
+ * ```
+ */
+export function createAlternation(locales: string[]) {
+  return locales.join("|");
 }
--- src/test/emit-runtime.test.ts
+++ src/test/emit-runtime.test.ts
@@ -1,9 +1,14 @@
 import { describe, expect, it } from "vitest";
-import { emitRuntime } from "../core/emit/runtime";
+import { createAlternation, createGlobPattern } from "../core/emit/runtime";
 
 describe("emitRuntime", () => {
   it("输入 ['en-US', 'zh-CN'] 返回 './{en-US,zh-CN}/*.ts'", () => {
-    const out = emitRuntime(["en-US", "zh-CN"]);
+    const out = createGlobPattern(["en-US", "zh-CN"]);
     expect(out).toBe("./{en-US,zh-CN}/*.ts");
+  });
+
+  it("输入 ['en-US', 'zh-CN'] 返回 'en-US|zh-CN'", () => {
+    const out = createAlternation(["en-US", "zh-CN"]);
+    expect(out).toBe("en-US|zh-CN");
   });
 });
