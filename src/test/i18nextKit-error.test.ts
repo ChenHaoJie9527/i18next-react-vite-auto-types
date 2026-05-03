f21997fc54a6ff531193d2038a96b018bc4d52bf
momotom <a18520145493@gmail.com>
2026-05-03 16:49:19 +0800

    feat: Creating i18nextKitError using unit test cases

---
+++ src/test/i18nextKit-error.test.ts
@@ -0,0 +1,30 @@
+import { describe, expect, it } from "vitest";
+import { I18nextKitError } from "../core";
+
+describe("I18nextKitError", () => {
+  it("创建错误时，code 和 message 正确", () => {
+    const error = new I18nextKitError(
+      "CONTRACTS_DIR_NOT_FOUND",
+      "The contracts directory does not exist"
+    );
+    expect(error.code).toBe("CONTRACTS_DIR_NOT_FOUND");
+    expect(error.message).toBe(
+      "[CONTRACTS_DIR_NOT_FOUND] The contracts directory does not exist"
+    );
+    expect(error.detail).toEqual({});
+  });
+
+  it("创建错误时，detail 正确", () => {
+    const error = new I18nextKitError(
+      "CONTRACTS_DIR_NOT_FOUND",
+      "The contracts directory does not exist",
+      { dir: "./contracts" }
+    );
+    console.log("error.detail =>", error.detail);
+    expect(error.code).toBe("CONTRACTS_DIR_NOT_FOUND");
+    expect(error.message).toBe(
+      "[CONTRACTS_DIR_NOT_FOUND] The contracts directory does not exist"
+    );
+    expect(error.detail).toEqual({ dir: "./contracts" });
+  });
+});
